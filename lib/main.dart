488f0b43a098729a486aec8230120f3f00fc6c1e
biamelo0 <beatrizmelo0952@gmail.com>
2024-05-23 11:29:32 -0300

    projetoCorecao

---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:projetoavaliacorrecao/Principal.dart';
+import 'package:flutter/material.dart';
+
+void main () => runApp(MaterialApp(
+  debugShowCheckedModeBanner: false,
+  home: Principal(),
+));
+
